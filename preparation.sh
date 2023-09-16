3ba79a237285b63fae36a573778c6f32e23d0119
ccartman <ccartman@student.21-school.ru>
2023-09-16 14:58:37 +0300

    Update preparation script.

---
+++ preparation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt -y install gcc
+sudo apt -y install autoconf
+sudo apt -y install gettext
+sudo apt -y install autopoint
+sudo apt -y install build-essential
+sudo apt -y install nginx
+sudo apt install -y stress
+
+sudo apt-get install -y apt-transport-https software-properties-common wget
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install grafana
+
+wget -O - https://deb.goaccess.io/gnugpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/goaccess.gpg >/dev/null
+$ echo "deb [signed-by=/usr/share/keyrings/goaccess.gpg arch=$(dpkg --print-architecture)] https://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/goaccess.list
+sudo apt-get update
+sudo apt-get -y install goaccess
+
