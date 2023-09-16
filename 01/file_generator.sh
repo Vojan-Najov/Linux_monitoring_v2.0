36bf33916c1d8745f405172e87cc73434cbfa110
ccartman <ccartman@student.21-school.ru>
2023-09-16 14:43:29 +0300

    refactore code.

--- 01/file_generator.sh
+++ 01/file_generator.sh
@@ -1,79 +1,78 @@
 #!/bin/bash
 
 function disk_is_full() {
-	cur_size=$( df --block-size=1 / | grep -v Filesystem | awk '{print $4}')
-	if [ $cur_size -le 1000000000 ]; then
-		return 0;
-	else
-		return 1;
-	fi
+  cur_size=$( df --block-size=1 / | grep -v Filesystem | awk '{print $4}')
+  if [ $cur_size -le 1000000000 ]; then
+    return 0;
+  else
+    return 1;
+  fi
 }
 
 function file_generator() {
-	directory=$1
-	subdirs_count=$2
-	dir_letters=$3
-	files_count=$4
-	file_letters=$5
-	ext=$6
-	file_size=$7
-	logfile=$8
-	subdir_name=$3
-	now=$(date +%d%m%y)
+  directory=$1
+  subdirs_count=$2
+  dir_letters=$3
+  files_count=$4
+  file_letters=$5
+  ext=$6
+  file_size=$7
+  logfile=$8
+  subdir_name=$3
+  now=$(date +%d%m%y)
 
-	: >"$logfile"
+  : >"$logfile"
 
-	while [ ${#subdir_name} -lt 4 ]; do
-		first_character=${subdir_name:0:1}
-		replace_characters=$first_character$first_character
-		subdir_name=${subdir_name/$first_character/$replace_characters}
-	done
+  while [ ${#subdir_name} -lt 4 ]; do
+    first_character=${subdir_name:0:1}
+    replace_characters=$first_character$first_character
+    subdir_name=${subdir_name/$first_character/$replace_characters}
+  done
 
-	dir_letters_idx=0
-	while [ $subdirs_count -gt 0 ]; do
-		if disk_is_full; then
-			break
-		fi
+  dir_letters_idx=0
+  while [ $subdirs_count -gt 0 ]; do
+    if disk_is_full; then
+      break
+    fi
 
-		mkdir -p "$directory""$subdir_name"_"$now"
+    mkdir -p "$directory""$subdir_name"_"$now" 2>/dev/null
 	
-		printf "%s " $(realpath "$directory""$subdir_name"_"$now""/") >>$logfile
-		printf "%s\n" "$( date +"%S/%M/%H/%d/%m/%Y" )" >>$logfile
+    printf "%s " $(realpath "$directory""$subdir_name"_"$now""/") >>$logfile
+    printf "%s\n" "$( date +"%S/%M/%H/%d/%m/%Y" )" >>$logfile
 
-		filename=$file_letters
+    filename=$file_letters
 
-		while [ ${#filename} -lt 4 ]; do
-			first_character=${filename:0:1}
-			replace_characters=$first_character$first_character
-			filename=${filename/$first_character/$replace_characters}
-		done
+    while [ ${#filename} -lt 4 ]; do
+      first_character=${filename:0:1}
+      replace_characters=$first_character$first_character
+      filename=${filename/$first_character/$replace_characters}
+    done
 
-		file_letters_idx=0
-		for i in $( seq 1 $files_count ); do
+    file_letters_idx=0
+    for i in $( seq 1 $files_count ); do
+      if disk_is_full; then
+        return
+      fi
 
-			if disk_is_full; then
-				return
-			fi
+      fullname="$directory""$subdir_name"_"$now""/""$filename"_"$now"."$ext"
 
-			fullname="$directory""$subdir_name"_"$now""/""$filename"_"$now"."$ext"
+      fallocate -l $file_size "$fullname"
 
-			fallocate -l $file_size "$fullname"
-
-			printf "%s " $(realpath "$fullname") >>$logfile
-			printf "%s " "$( date +"%S/%M/%H/%d/%m/%Y" )" >>$logfile
-			printf "%s\n" "$file_size""B" >>$logfile
+      printf "%s " $(realpath "$fullname") >>$logfile
+      printf "%s " "$( date +"%S/%M/%H/%d/%m/%Y" )" >>$logfile
+      printf "%s\n" "$file_size""B" >>$logfile
 			
-			character=${file_letters:$file_letters_idx:1}
-			replace_characters=$character$character
-			filename=${filename/$character/$replace_characters}
-			(( file_letters_idx = ($file_letters_idx + 1) % ${#file_letters} ))
-		done
+      character=${file_letters:$file_letters_idx:1}
+      replace_characters=$character$character
+      filename=${filename/$character/$replace_characters}
+      (( file_letters_idx = ($file_letters_idx + 1) % ${#file_letters} ))
+    done
 
-		character=${dir_letters:$dir_letters_idx:1}
-		replace_characters=$character$character
-		subdir_name=${subdir_name/$character/$replace_characters}
-		(( dir_letters_idx = ($dir_letters_idx + 1) % ${#dir_letters} ))
-		(( subdirs_count = $subdirs_count - 1 ))
-	done
+    character=${dir_letters:$dir_letters_idx:1}
+    replace_characters=$character$character
+    subdir_name=${subdir_name/$character/$replace_characters}
+    (( dir_letters_idx = ($dir_letters_idx + 1) % ${#dir_letters} ))
+    (( subdirs_count = $subdirs_count - 1 ))
+  done
 }
 
--- 01/main.sh
+++ 01/main.sh
@@ -6,73 +6,73 @@ source "$(dirname $0)"/is_natural_number.sh
 source "$(dirname $0)"/file_generator.sh
 
 if [ $# -ne 6 ]; then
-	echo "Error: 6 arguments are expected."
-	echo "$( print_usage )"
-	exit 1
+  echo "Error: 6 arguments are expected."
+  echo "$( print_usage )"
+  exit 1
 fi
 
 directory="$1"
 if [ ${directory: -1} != "/" ]; then
-	directory="$directory""/"
+  directory="$directory""/"
 fi
 if [ ! -d "$directory" ]; then
   echo "Error: the directory $1 does not exist."
-	exit 1
+  exit 1
 fi
-if [ ! -x "$directory" ]; then
-  echo "Error: no permissions to write to the directory $1."
-	exit 1;
+if [ ! -x "$directory" ] || [ ! -w "$directory" ] ; then
+  echo "Error: no permissions to write to the directory '$directory'."
+  exit 1
 fi
 
 subdirs_count="$2"
 if ! is_natural_number "$subdirs_count"; then
   echo "Error: natural number are expected for number of subfolders."
-	exit 1;
+  exit 1
 fi
 
 dir_letters="$3"
 if ! is_alphabet "$dir_letters"; then
   echo "Error: alphabet letters are expected for subfolder's name.";
-	exit 1;
+  exit 1
 fi
 if [ ${#dir_letters} -gt 7 ]; then
   echo "Error: no more than 7 letters are expected for subfolder's name."
-	exit 1;
+  exit 1
 fi
 
 files_count="$4"
 if ! is_natural_number "$files_count"; then
   echo "Error: natural number are expected for number of files."
-	exit 1;
+  exit 1
 fi
 
 file_letters=$( cut -d'.' -f1 <<< "$5" )
 ext_letters=$( cut -d'.' -f2 <<< "$5" )
 if ! [[ $5 =~ ^[a-zA-Z]+\.[a-zA-Z]+$ ]]; then
   echo "Error: [a-zA-Z].[a-zA-z] are expected for file's name."
-	exit 1
+  exit 1
 fi
 if ! (is_alphabet "$file_letters" && is_alphabet "$ext_letters"); then
   echo "Error: [a-zA-Z].[a-zA-z] are expected for file's name."
-	exit 1
+  exit 1
 fi
 if [ ${#file_letters} -gt 7 ] || [ ${#ext_letters} -gt 3 ]; then
-	echo "Error: to the left of the dot, no more than 7 letters are "
+  echo "Error: to the left of the dot, no more than 7 letters are "
   echo "       expected for the filename, to the right of the dot,"
   echo "       no more than 3 letters are expected for the extension."
-  exit 1;
+  exit 1
 fi
 
 file_size="$6"
 KB=$(echo ${file_size: -2} | awk '{print tolower($0)}')
 if [ "$KB" != "kb" ]; then
-	echo "Error: expected kb dimension for file's size argument."
-	exit 1
+  echo "Error: expected kb dimension for file's size argument."
+  exit 1
 fi
 file_size=${file_size:0:-2}
 if ! is_natural_number "$file_size" || [ $file_size -gt 100 ]; then
-	echo "Error: incorrect file's size (natural number not more than 100)."
-	exit 1
+  echo "Error: incorrect file's size (natural number not more than 100)."
+  exit 1
 fi
 (( file_size = $file_size * 1000 ))
 
